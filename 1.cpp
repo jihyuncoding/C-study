82586f02fa79dbb43ea99b5d61e884587fad52f9
Park jihyun <[thotemily@naver.com]>
2023-04-03 20:34:59 +0900

    초기 커밋.

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter a degree in Celsius : ";
+	double celsius;
+	cin >> celsius;
+
+	double farenheit = (9 / 5) * celsius + 32;
+
+
+
+}
